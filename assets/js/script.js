00633956b651759168673c9953bd8ea8e0a11cd2
General Zelgius <mattlow123@gmail.com>
2022-01-25 18:03:27 -0500

    added functionality to the trash drop zone

--- assets/js/script.js
+++ assets/js/script.js
@@ -225,9 +225,23 @@ $(".card .list-group").sortable({
     // update array on tasks object and save
     tasks[arrName] = tempArr;
     saveTasks();
-    
-    console.log(tempArr)
   }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+
 });
 
 // load tasks for the first time
